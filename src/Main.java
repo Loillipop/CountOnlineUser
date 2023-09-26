0ffe680da8327c10515d09dafe3805aacc97c210
lolipop <spoon9004@gmail.com>
2023-09-27 01:36:07 +0200

    test repo create

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import com.packag.UserOnline;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class Main {
+
+    static final int startYear = 2020;
+    static final int endYear = 2022;
+
+    public static void main(String[] args) {
+
+    }
+
+    public int findMaxOnline() {
+        // Найти самое большое количество онлайна одновременно
+
+        Random random = new Random();
+        int i = random.nextInt(4, 7);
+
+        List<UserOnline> onlineList = Stream.generate(Main::generateUser).limit(i).toList();
+        return 0;
+    }
+
+    public LocalDate findMaxOnlineDate() {
+        // Найти дату самого большого онлайна(первый день в диапазоне, когда было одновременно онлайн самое большое кол-во людей)
+        // Более сложный вариант, это найти диапазон дат наибольшего онлайна
+        // (то есть к примеру дата начала самого большого онлайна 05.05.2023, дата завершения 10.07.2023)
+
+        Random random = new Random();
+        int i = random.nextInt(4, 7);
+
+        List<UserOnline> onlineList = Stream.generate(Main::generateUser).limit(i).toList();
+
+        return null;
+    }
+
+    public static UserOnline generateUser() {
+        Random random = new Random();
+
+        int randomYearStart = random.nextInt(startYear, endYear);
+        int randomMonthStart = random.nextInt(1, 12);
+        int randomDayStart = random.nextInt(1, Month.of(randomMonthStart).maxLength());
+        int randomYearEnd = random.nextInt(startYear, endYear);
+        int randomMonthEnd = random.nextInt(1, 12);
+        int randomDayEnd = random.nextInt(1, Month.of(randomMonthEnd).maxLength());
+
+        LocalDate startUserOnline = LocalDate.of(randomYearStart, randomMonthStart, randomDayStart);
+        LocalDate endUserOnline = LocalDate.of(randomYearEnd, randomMonthEnd, randomDayEnd);
+
+        if (startUserOnline.isAfter(endUserOnline)) {
+            LocalDate temp = startUserOnline;
+            startUserOnline = endUserOnline;
+            endUserOnline = temp;
+        }
+
+        return new UserOnline(startUserOnline, endUserOnline);
+    }
+}
